62d57a0097ae76909d7d0cb1019f7fa278d8810f
Kaisikk <Kosikov.Ilya@mail.ru>
2024-03-11 22:21:16 +0400

    Работа с правами пользователя и добавление функционала для редактирования пользователей

--- src/main/java/com/kaisikk/java/springboot/javaspringboot/domain/Role.java
+++ src/main/java/com/kaisikk/java/springboot/javaspringboot/domain/Role.java
@@ -3,7 +3,7 @@ package com.kaisikk.java.springboot.javaspringboot.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
 
     @Override
